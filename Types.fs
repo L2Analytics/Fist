1f3a4956fedec6a36694dc9ba8235e3d77a39d4e
Martin J. Lenardon <martin.lenardon@L2Analytics.com>
2017-01-25 19:56:16 -0700

    More cleanup

--- Types.fs
+++ Types.fs
@@ -17,18 +17,3 @@ type IPredictor<'X, 'Y> =
 
 type IModel<'X, 'Y, 'P when 'P :> IPredictor<'X, 'Y>> =
     abstract Fit: ('X*'Y) array -> 'P
-
-
-type IAdult =
-    abstract member DoAdultThing: unit -> string
-
-type IChild<'T when 'T :> IAdult>=
-    abstract member GrowUp: unit -> 'T
-
-type IGrownDog =
-    inherit IAdult
-    abstract member Bark: unit -> string
-
-type IPuppy =
-    inherit IChild<IGrownDog>
-    abstract member DoPuppyThing: unit -> string
